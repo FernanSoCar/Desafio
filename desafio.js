4391da90d434de7dedefcfc091462d23fabf4d76
FernanSoCar <fernandosousaofficial@hotmail.com>
2024-05-02 23:49:01 -0300

    desafio concluido

---
+++ desafio.js
@@ -0,0 +1,22 @@
+let nome = "Fernando"
+let xp = 10000
+
+console.log("O Herói de nome " + nome, "está no nível de ")
+
+if (xp <= 1000) {
+    console.log("Ferro")
+} else if (xp <= 2000) {
+    console.log("Bronze")
+} else if (xp <= 5000) {
+    console.log("Prata")
+} else if (xp <= 7000) {
+    console.log("Ouro")
+} else if (xp <= 8000) {
+    console.log("Platina")
+} else if (xp <= 9000) {
+    console.log("Ascendente")
+} else if (xp <= 10000) {
+    console.log("Imortal")
+} else {
+    console.log("Radiante")
+}
